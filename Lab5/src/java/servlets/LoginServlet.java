cdd730efc9320c7adf6c2f3f0fb88aee317a3f9c
Sean Zhu <seanzhu@live.com>
2021-10-08 23:09:39 -0600

    Created 3 servlets

--- Lab5/src/java/servlets/LoginServlet.java
+++ Lab5/src/java/servlets/LoginServlet.java
@@ -17,14 +17,18 @@ public class LoginServlet extends HttpServlet {
          @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        // displays login form
+        // responsible for logging out the user
+            // invalidate the session
+            // display a message "successfully logged out"
         
     }
    
           @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        // validates that username and password not empty
+        // pass the username and password parameters to login() - AccountService
         
     }
     
